9258428ec028bf871f7a5d9ca521cbb7d6323cdb
Arnaud <bandonkeyea@gmail.com>
2022-05-31 03:20:49 -0600

    Display the calculator

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import Header from './header/header';
+import Calculator from './components/calculator';
 
-const App = () => (
-  <div className="App" />
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render = () => (
+    <>
+      <Header />
+      <Calculator />
+    </>
+  )
+}
+
+// const App = () => (
+//   <div className="App">
+//     <Calculator />
+//   </div>
+// );
 
 export default App;
---
+++ src/header/header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+class Header extends React.PureComponent {
+    render = () => <div className="output">0</div>;
+}
+
+export default Header;
