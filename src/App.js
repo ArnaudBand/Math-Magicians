17874b5463c831a5327afec2c77cd1c38997531a
Arnaud <bandonkeyea@gmail.com>
2022-06-07 16:54:57 -0600

    Add Home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import {
-  HashRouter as Router, Routes, Route, Link,
+  Routes, Route, Link,
 } from 'react-router-dom';
-import Header from './components/Home';
+import Home from './components/Home';
 import Calculator from './components/calculator';
 
 const App = () => (
   <div className="calculator">
-    <Router>
-      <header className="header">
-        <h2 className="logo">Math Magicians</h2>
-        <Link to="/">Home</Link>
-        <Link to="/calculator">Calculator</Link>
-      </header>
-      <Routes>
-        <Route path="/" element={<Header />} />
-        <Route path="/calculator" element={<Calculator />} />
-      </Routes>
-    </Router>
-    <Calculator />
+    <header className="header">
+      <h2 className="logo">Math Magicians</h2>
+      <Link to="/">Home</Link>
+      <Link to="/calculator">Calculator</Link>
+    </header>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/calculator" element={<Calculator />} />
+    </Routes>
   </div>
 );
 
---
+++ src/components/Home.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const Home = () => (
+  <>
+    <header className="div_header">
+      <h1 className="logo">Welcome to our page</h1>
+      <p>
+        In hac habitasse platea dictumst vestibulum. Aliquet porttitor lacus luctus accumsan.
+        Scelerisque purus semper eget duis at tellus at. In fermentum et sollicitudin ac orci
+        phasellus egestas tellus. Cursus turpis massa tincidunt dui ut ornare. Id leo in vitae
+        turpis
+        massa sed elementum tempus egestas. Vestibulum mattis ullamcorper
+        velit sed ullamcorper morbi
+        tincidunt ornare massa. Ultrices eros in cursus turpis massa tincidunt dui. Iaculis at erat
+        pellentesque adipiscing commodo elit. Vitae ultricies leo integer malesuada nunc vel. Massa
+        massa ultricies mi quis hendrerit dolor magna. Nibh tortor
+        id aliquet lectus proin nibh nisl.
+        Est sit amet facilisis magna etiam tempor orci eu. Ut sem viverra aliquet eget sit amet
+        tellus. Pharetra massa massa ultricies mi quis hendrerit dolor magna.
+      </p>
+      <p>
+        In hac habitasse platea dictumst vestibulum. Aliquet porttitor lacus luctus accumsan.
+        Scelerisque purus semper eget duis at tellus at. In fermentum et sollicitudin ac orci
+        phasellus egestas tellus. Cursus turpis
+        massa tincidunt dui ut ornare. Id leo in vitae turpis
+        massa sed elementum tempus egestas. Ve
+        stibulum mattis ullamcorper velit sed ullamcorper morbi
+        tincidunt ornare massa. Ultrices eros in cursus turpis massa tincidunt dui. Iaculis at erat
+        pellentesque adipiscing commodo elit
+        . Vitae ultricies leo integer malesuada nunc vel. Massa
+        massa ultricies mi quis hendrerit
+        dolor magna. Nibh tortor id aliquet lectus proin nibh nisl.
+        Est sit amet facilisis magna etiam tempor orci eu. Ut sem viverra aliquet eget sit amet
+        tellus. Pharetra massa massa ultricies mi quis hendrerit dolor magna.
+      </p>
+    </header>
+  </>
+);
+
+export default Home;
