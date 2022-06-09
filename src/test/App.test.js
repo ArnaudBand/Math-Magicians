4c5adccfc81e4c5d77aacf3d109c0f7ea8c2820e
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-09 09:48:16 +0500

    Update App.js

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import { MemoryRouter } from 'react-router-dom'
-import App from '../App'
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import App from '../App';
 
 it('App renders correctly', () => {
+  // eslint-disable-next-line no-undef
   const tree = renderer
     .create(
       <MemoryRouter>
         <App />
-      </MemoryRouter>
+      </MemoryRouter>,
     )
     .toJSON();
   expect(tree).toMatchSnapshot();
-})
+});
