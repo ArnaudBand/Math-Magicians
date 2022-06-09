9e230830755ae95ea4952157de66fe4929ef99d6
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-09 09:42:21 +0500

    Fix errors

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,14 +1,1 @@
-import renderer from 'react-test-renderer';
-import { MemoryRouter } from 'react-router-dom';
-import App from '../App';
 
-it('App renders correctly', () => {
-  const tree = renderer
-    .create(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
-    )
-    .toJSON();
-  expect(tree).toMatchSnapshot();
-});
