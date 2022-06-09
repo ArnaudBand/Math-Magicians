e8e1bc9a5020566969445776fdeebabd28a27b26
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-09 13:58:10 +0500

    Update button.test.js

--- src/test/button.test.js
+++ src/test/button.test.js
@@ -1,1 +1,8 @@
+import renderer from 'react-test-renderer';
+import Button from '../components/button';
 
+it('Calculator button renders correctly', () => {
+  const handleClick = () => true;
+  const tree = renderer.create(<Button text="Submit" handleClick={handleClick} />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
