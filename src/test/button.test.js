392b6fb1680467b51e822aea951a7f46cf89c0c8
Arnaud <bandonkeyea@gmail.com>
2022-06-09 03:20:56 -0600

    Solve button test

--- src/test/button.test.js
+++ src/test/button.test.js
@@ -1,8 +1,10 @@
 import renderer from 'react-test-renderer';
 import Button from '../components/button';
 
+//  This is just an example but you can solve it the way you want
+
 it('Calculator button renders correctly', () => {
   const handleClick = () => true;
-  const tree = renderer.create(<Button text="Submit" handleClick={handleClick} />).toJSON();
+  const tree = renderer.create(<Button id="1" className="class" text="Submit" onClick={handleClick} />).toJSON();
   expect(tree).toMatchSnapshot();
 });
